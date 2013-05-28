4637b59356c2257053bb97f01902c60a7f6cb3e7
root <root@snf-30497.vm.okeanos.grnet.gr>
2013-05-28 15:26:25 +0300

    Added serial write ability on specified COM

---
+++ Serial.bat
@@ -0,0 +1,10 @@
+@echo off
+cls
+echo Simple COM Interface for DOS
+SET/P COM=Input port number (ex.: 1):
+MODE COM%COM%: baud=9600 parity=N data=8 stop=1
+:LOOP
+    SET/P content=Input data to send:
+    echo %content% > com%COM%
+    goto LOOP
+
